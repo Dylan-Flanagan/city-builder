a4c77d77abad96cb519b658e86410d3a1d9cd8b2
Dylan <dylflanagan@gmail.com>
2022-10-16 22:32:16 -0700

    Fixed display of climate/architecure selections

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 /* Imports */
 
 /* Get DOM Elements */
-const nameInput = document.getElementById('header');
-const aestheticSelect = document.getElementById('aesthetic-select');
-const vibeSelect = document.getElementById('vibe-select');
+const nameInput = document.getElementById('name-input');
+const climateSelect = document.getElementById('climate-select');
+const architectureSelect = document.getElementById('architecture-select');
 const nameDisplay = document.getElementById('name-display');
-const aestheticImage = document.getElementById('aesthetic-image');
-const vibeImage = document.getElementById('vibe-image');
+const climateImage = document.getElementById('climate-display');
+const architectureImage = document.getElementById('architecture-display');
 const builderArticle = document.getElementById('builder-article');
 const placeInput = document.getElementById('place-input');
 const addButton = document.getElementById('add-button');
@@ -15,25 +15,54 @@ const attractionList = document.getElementById('attraction-list');
 /* State */
 const city = {
     name: 'Oblivion',
-    aesthetic: 'ugly',
-    vibe: 'boxes',
+    climate: 'ugly',
+    architecture: 'boxes',
     attractions: [],
 };
+
+/* Events */
 nameInput.addEventListener('input', () => {
     city.name = nameInput.value;
+    displayPlaces();
+});
+
+climateSelect.addEventListener('change', () => {
+    city.climate = climateSelect.value;
     displayCity();
 });
-/* Events */
+
+architectureSelect.addEventListener('change', () => {
+    city.architecture = architectureSelect.value;
+    displayCity();
+});
 
 /* Display Functions */
-function displayPlaces() {
-    // clear the list
-    attractionList.innerHTML = '';
+function displayControls() {
+    nameInput.value = city.name;
+    climateSelect.value = city.climate;
+    architectureSelect.value = city.architecture;
+}
 
-    for (const attraction of city.attractions) {
-        const li = document.createElement('li');
-        li.textContent = attraction;
-        attractionList.append(li);
-    }
+function displayCity() {
+    builderArticle.classList.value = '';
+    builderArticle.classList.add(city.climate, city.architecture);
+    nameDisplay.textContent = city.name;
+    climateImage.src = 'assets/' + city.climate + '.png';
+    climateImage.alt = city.climate;
+    architectureImage.src = 'assets/' + city.architecture + '.png';
+    architectureImage.alt = city.architecture;
+}
+
+function displayPlaces() {
+    nameDisplay.textContent = city.name;
+    // clear the list
+    // attractionList.innerHTML = '';
+    // for (const attraction of city.attractions) {
+    //     const li = document.createElement('li');
+    //     li.textContent = attraction;
+    //     attractionList.append(li);
+    // }
 }
 // (don't forget to call any display functions you want to run on page load!)
+displayControls();
+displayCity();
